0ae02a190a0cdd444c9ade546e5b2ff44d3d533f
lebrancconvas <lebrancconvas@gmail.com>
2022-01-09 03:41:26 +0700

    🔨 Seperate ImageData to the individual file.

--- src/ImageDownloading/GetImage.js
+++ src/ImageDownloading/GetImage.js
@@ -1,24 +1,14 @@
 const puppeteer = require('puppeteer');
+const ImageData = require('./ImageData');
 const ImageDownloader = require('./ImageDownloader');
-
-const ImageData = {
-    googleimage: {
-        url: "https://google.com",
-        imageselector: "body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img"
-    },
-    keyakilogoimage: {
-        url: "http://stage48.net/wiki/index.php/Keyakizaka46",
-        imageselector: "#mw-content-text > div > table.toccolours.itwiki_template_toc > tbody > tr:nth-child(3) > td > a > img"
-    }
-};
 
 const GetImage = async() => {
 
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-    const link = await ImageData.keyakilogoimage.url;
-    const imageSelector = await ImageData.keyakilogoimage.imageselector;
+    const link = ImageData.keyakilogoimage.url;
+    const imageSelector = ImageData.keyakilogoimage.imageselector;
 
     await page.goto(link);
     const imageURL = await page.$eval(imageSelector, img => img.src);
---
+++ src/ImageDownloading/ImageData.js
@@ -0,0 +1,12 @@
+const ImageData = {
+    googleimage: {
+        url: "https://google.com",
+        imageselector: "body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img"
+    },
+    keyakilogoimage: {
+        url: "http://stage48.net/wiki/index.php/Keyakizaka46",
+        imageselector: "#mw-content-text > div > table.toccolours.itwiki_template_toc > tbody > tr:nth-child(3) > td > a > img"
+    }
+};
+
+module.exports = ImageData;
