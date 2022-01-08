f88c80dd151e5bdbb0c4f478d0c058e92bff9b80
lebrancconvas <lebrancconvas@gmail.com>
2022-01-09 02:09:54 +0700

    🔨 Structuring Web Scraping Script.

--- src/BasicScraping.js
+++ src/BasicScraping.js
@@ -3,4 +3,12 @@ const puppeteer = require('puppeteer');
 const BasicScraping = async() => {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
+
+    const link = `https://google.com`;
+    const selector = ``;
+
+    await page.goto(link);
+
+    await page.close();
+    await browser.close();
 }
