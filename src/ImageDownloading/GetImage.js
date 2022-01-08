636ed925c1b8540cdb781266d3c46d69ce8b5fa8
lebrancconvas <lebrancconvas@gmail.com>
2022-01-09 03:32:39 +0700

    🔨 Add Object for storing the informations of image in website.

--- src/ImageDownloading/GetImage.js
+++ src/ImageDownloading/GetImage.js
@@ -1,12 +1,24 @@
 const puppeteer = require('puppeteer');
 const ImageDownloader = require('./ImageDownloader');
 
+const ImageData = {
+    googleimage: {
+        url: "https://google.com",
+        imageselector: "body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img"
+    },
+    keyakilogoimage: {
+        url: "http://stage48.net/wiki/index.php/Keyakizaka46",
+        imageselector: "#mw-content-text > div > table.toccolours.itwiki_template_toc > tbody > tr:nth-child(3) > td > a > img"
+    }
+};
+
 const GetImage = async() => {
+
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-    const link = `https://google.com`;
-    const imageSelector = `body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img`;
+    const link = await ImageData.keyakilogoimage.url;
+    const imageSelector = await ImageData.keyakilogoimage.imageselector;
 
     await page.goto(link);
     const imageURL = await page.$eval(imageSelector, img => img.src);
