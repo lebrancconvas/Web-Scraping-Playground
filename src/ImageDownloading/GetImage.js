32fa80193dd34033391d39b2a3deb17c77afbafc
lebrancconvas <lebrancconvas@gmail.com>
2022-01-09 19:05:33 +0700

    💡 Add Get Image Algorithm Comment.

--- src/ImageDownloading/GetImage.js
+++ src/ImageDownloading/GetImage.js
@@ -3,24 +3,28 @@ const ImageData = require('../../data/input/js/ImageData');
 const ImageDownloader = require('./ImageDownloader');
 
 const GetImage = async() => {
-
+    // Init Browser and Page via Puppeteer. 
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
+    // Declare website link and CSS Selector. 
     const link = ImageData.songhyekyoimage.url;
     const imageSelector = ImageData.songhyekyoimage.imageselector;
 
+    // Go to the website.
     await page.goto(link);
+
+    // Get Image URL from CSS Selector.
     const imageURL = await page.$eval(imageSelector, img => img.src);
 
+    // Implementation Image Downloader Function. 
     ImageDownloader(imageURL, filename => {
         console.log("Downloading Success.");
     })
 
+    // Exit from Page and Browser. 
     await page.close();
     await browser.close();
-
-    // return `The Image URL: ${imageURL}`;
 }
 
 module.exports = GetImage;
